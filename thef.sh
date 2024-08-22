19bf9be807d345f97d711113c68fc9269045a53e
TheF <wassimbolles18@gmail.com>
2024-08-22 21:50:26 +0100

    no_need_to_set_a_commit

--- thef.sh
+++ thef.sh
@@ -6,7 +6,7 @@
 #    By: wabolles <wabolles@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/08/19 16:34:39 by wabolles          #+#    #+#              #
-#    Updated: 2024/08/20 08:06:50 by wabolles         ###   ########.fr        #
+#    Updated: 2024/08/20 11:51:51 by wabolles         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,20 +25,20 @@ bash ~/maconfigs/dark_mode.sh
 # killall SystemUIServer
 # killall Dock
 
-# 3. Change the display color profile to Rec. ITU-R BT.709-5
-# osascript <<EOF
-# tell application "System Preferences"
-#     reveal anchor "displaysDisplayTab" of pane id "com.apple.preference.displays"
-# end tell
+#3. Change the display color profile to Rec. ITU-R BT.709-5
+osascript <<EOF
+tell application "System Preferences"
+    reveal anchor "displaysDisplayTab" of pane id "com.apple.preference.displays"
+end tell
 
-# tell application "System Events"
-#     tell process "System Preferences"
-#         delay 1
-#         click button "Color" of tab group 1 of window 1
-#         delay 1
-#         select (row 1 of table 1 of scroll area 1 of tab group 1 of window 1 whose value of text field 1 of UI element 1 of UI element 1 is "Rec. ITU-R BT.709-5")
-#     end tell
-# end tell
+tell application "System Events"
+    tell process "System Preferences"
+        delay 1
+        click button "Color" of tab group 1 of window 1
+        delay 1
+        select (row 1 of table 1 of scroll area 1 of tab group 1 of window 1 whose value of text field 1 of UI element 1 of UI element 1 is "Rec. ITU-R BT.709-5")
+    end tell
+end tell
 
 # tell application "System Preferences" to quit
 # EOF
